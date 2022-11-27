7ca1c5a226bd46579c1531c0a40b1869ecdbac74
pear651530 <94907326+pear651530@users.noreply.github.com>
2022-11-27 15:29:29 +0800

    Add files via upload

---
+++ try.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>首頁</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        .fakeimg {
+            height: 200px;
+            background: #aaa;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="jumbotron text-center" style="margin-bottom:0">
+        <h1>吾愛吾師，但吾更愛電競</h1>
+        <p>study is a work but game is my life</p>
+    </div>
+    <div class="container" style="margin-top:30px">
+        <div class="row">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-4">
+                <div class="fakeimg">Fake Image</div>
+            </div>
+            <div class="col-sm-6">
+                <h2>About Me</h2>
+                <h5>Photo of me:</h5>
+            </div>
+
+        </div>
+    </div>
+
+</body>
+
+</html>
