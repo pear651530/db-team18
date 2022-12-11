4132431fb5ccfb551ab60c9f1655f1993dea28f2
Johnny0225221 <100283867+Johnny0225221@users.noreply.github.com>
2022-12-11 14:33:50 +0800

    Add files via upload

---
+++ insert_contest.php
@@ -0,0 +1,19 @@
+<?php
+    header("Content-type:text/html;charset=utf-8");
+    include_once "team18_database.php";
+
+    $game_name=$_POST["game_name"];
+    $region=$_POST["region"];
+    $month=$_POST["month"];
+    $date=$_POST["date"];
+    $time=$_POST["time"];
+    $team1=$_POST["team1"];
+    $team2=$_POST["team2"];
+    $winteam=$_POST["winteam"];
+    $query=("insert into game values(?,?,?,?,?,?,?,?)");
+    $stmt=$db->prepare($query);
+    $stmt->execute(array($game_name,$region,$month,$date,
+                    $time,$team1,$team2,$winteam));
+    //header("Location:infrom.php");
+
+?>
