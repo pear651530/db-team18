56f0cbfbaee457d206d00f04ca2d3806caef34ea
Johnny0225221 <100283867+Johnny0225221@users.noreply.github.com>
2022-12-18 15:12:21 +0800

    Update insert.php

--- insert.php
+++ insert.php
@@ -95,6 +95,104 @@
                         echo "<option value='" . $result1[$i]['team'] . "'>" . $result1[$i]['team'] . "</option>";
                     }
                     echo "</select></div><div class='col-sm-3'></div></div>";
+                }else if ($table_name == "region_name") {
+                    $query = ("select distinct game_name from game");
+                    $stmt = $db->prepare($query);
+                    $stmt->execute();
+                    $result = $stmt->fetchAll();
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>遊戲名稱: </div><div class='col-sm-4'><select name='data1'>";
+                    for ($i = 0; $i < count($result); $i++) {
+                        echo "<option value='" . $result[$i]['game_name'] . "'>" . $result[$i]['game_name'] . "</option>";
+                    }
+                    echo "</select></div><div class='col-sm-3'></div></div>";
+
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>賽區:</div><div class='col-sm-4'>";
+                    echo "<input type='text' placeholder='LCK' name='data2' style='border: 2px solid black; background-color: white; width: 300px;'></div><div class='col-sm-3'></div></div>";
+                    
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>賽季:</div><div class='col-sm-4'>";
+                    echo "<input type='text' placeholder='spring' name='data3' style='border: 2px solid black; background-color: white; width: 300px;'></div><div class='col-sm-3'></div></div>";
+                    
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>begin_month:</div><div class='col-sm-4'>";
+                    echo "<input type='text' placeholder='1' name='data4' style='border: 2px solid black; background-color: white; width: 300px;'></div><div class='col-sm-3'></div></div>";
+                    
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>begin_date:</div><div class='col-sm-4'>";
+                    echo "<input type='text' placeholder='21' name='data5' style='border: 2px solid black; background-color: white; width: 300px;'></div><div class='col-sm-3'></div></div>";
+                
+                    
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>end_month:</div><div class='col-sm-4'>";
+                    echo "<input type='text' placeholder='4' name='data6' style='border: 2px solid black; background-color: white; width: 300px;'></div><div class='col-sm-3'></div></div>";
+                    
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>end_date:</div><div class='col-sm-4'>";
+                    echo "<input type='text' placeholder='21' name='data7' style='border: 2px solid black; background-color: white; width: 300px;'></div><div class='col-sm-3'></div></div>";
+                   
+                }else if ($table_name == "contest") {
+                    $query = ("select distinct game_name from game");
+                    $stmt = $db->prepare($query);
+                    $stmt->execute();
+                    $result = $stmt->fetchAll();
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>遊戲名稱: </div><div class='col-sm-4'><select name='data1'>";
+                    for ($i = 0; $i < count($result); $i++) {
+                        echo "<option value='" . $result[$i]['game_name'] . "'>" . $result[$i]['game_name'] . "</option>";
+                    }
+                    echo "</select></div><div class='col-sm-3'></div></div>";
+
+                    $query1 = ("select distinct region from region_name");
+                    $stmt = $db->prepare($query1);
+                    $stmt->execute();
+                    $result1 = $stmt->fetchAll();
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>賽區: </div><div class='col-sm-4'><select name='data2'>";
+                    for ($i = 0; $i < count($result1); $i++) {
+                        echo "<option value='" . $result1[$i]['region'] . "'>" . $result1[$i]['region'] . "</option>";
+                    }
+                    echo "</select></div><div class='col-sm-3'></div></div>";
+                    
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>month:</div><div class='col-sm-4'>";
+                    echo "<input type='text' placeholder='1' name='data3' style='border: 2px solid black; background-color: white; width: 300px;'></div><div class='col-sm-3'></div></div>";
+                    
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>date:</div><div class='col-sm-4'>";
+                    echo "<input type='text' placeholder='25' name='data4' style='border: 2px solid black; background-color: white; width: 300px;'></div><div class='col-sm-3'></div></div>";
+                    
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>time:</div><div class='col-sm-4'>";
+                    echo "<input type='text' placeholder='1600' name='data5' style='border: 2px solid black; background-color: white; width: 300px;'></div><div class='col-sm-3'></div></div>";
+                    
+                    $query2 = ("select distinct team from team_info");
+                    $stmt = $db->prepare($query2);
+                    $stmt->execute();
+                    $result2 = $stmt->fetchAll();
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>team1: </div><div class='col-sm-4'><select name='data6'>";
+                    for ($i = 0; $i < count($result2); $i++) {
+                        echo "<option value='" . $result2[$i]['team'] . "'>" . $result2[$i]['team'] . "</option>";
+                    }
+                    echo "</select></div><div class='col-sm-3'></div></div>";
+
+                    $query3 = ("select distinct team from team_info");
+                    $stmt = $db->prepare($query3);
+                    $stmt->execute();
+                    $result3 = $stmt->fetchAll();
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>team2: </div><div class='col-sm-4'><select name='data7'>";
+                    for ($i = 0; $i < count($result3); $i++) {
+                        echo "<option value='" . $result3[$i]['team'] . "'>" . $result3[$i]['team'] . "</option>";
+                    }
+                    echo "</select></div><div class='col-sm-3'></div></div>";
+
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>win_team:</div><div class='col-sm-4'>";
+                    echo "<input type='text' placeholder='BYG' name='data8' style='border: 2px solid black; background-color: white; width: 300px;'></div><div class='col-sm-3'></div></div>";
+                   
+                }else if ($table_name == "team_info") {
+                    $query = ("select distinct game_name from game");
+                    $stmt = $db->prepare($query);
+                    $stmt->execute();
+                    $result = $stmt->fetchAll();
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>遊戲名稱: </div><div class='col-sm-4'><select name='data1'>";
+                    for ($i = 0; $i < count($result); $i++) {
+                        echo "<option value='" . $result[$i]['game_name'] . "'>" . $result[$i]['game_name'] . "</option>";
+                    }
+                    echo "</select></div><div class='col-sm-3'></div></div>";
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>team:</div><div class='col-sm-4'>";
+                    echo "<input type='text' placeholder='T1' name='data2' style='border: 2px solid black; background-color: white; width: 300px;'></div><div class='col-sm-3'></div></div>";
+                    echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-2'>location:</div><div class='col-sm-4'>";
+                    echo "<input type='text' placeholder='Korea' name='data3' style='border: 2px solid black; background-color: white; width: 300px;'></div><div class='col-sm-3'></div></div>";
+                    
                 }
                 echo "<div class='row'><div class='col-sm-3'></div><div class='col-sm-6'><input type='submit' name='submit' value='新增'>";
                 echo "</div><div class='col-sm-3'></div></div>";
@@ -124,6 +222,39 @@
                     $query = ("insert into player values(?,?,?,?)");
                     $stmt = $db->prepare($query);
                     $stmt->execute(array($data2, $data3, $data4, $data1));
+                }
+                else if ($table_name == "region_name") {
+                    $data1 = $_POST["data1"];
+                    $data2 = $_POST["data2"];
+                    $data3 = $_POST["data3"];
+                    $data4 = $_POST["data4"];
+                    $data5 = $_POST["data5"];
+                    $data6 = $_POST["data6"];
+                    $data7 = $_POST["data7"];
+                    $query = ("insert into region_name values(?,?,?,?,?,?,?)");
+                    $stmt = $db->prepare($query);
+                    $stmt->execute(array($data1, $data2, $data3, $data4,$data5, $data6, $data7));
+                }
+                else if ($table_name == "contest") {
+                    $data1 = $_POST["data1"];
+                    $data2 = $_POST["data2"];
+                    $data3 = $_POST["data3"];
+                    $data4 = $_POST["data4"];
+                    $data5 = $_POST["data5"];
+                    $data6 = $_POST["data6"];
+                    $data7 = $_POST["data7"];
+                    $data8 = $_POST["data8"];
+                    $query = ("insert into contest values(?,?,?,?,?,?,?,?)");
+                    $stmt = $db->prepare($query);
+                    $stmt->execute(array($data1, $data2, $data3, $data4,$data5, $data6, $data7, $data8));
+                }
+                else if ($table_name == "team_info") {
+                    $data1 = $_POST["data1"];
+                    $data2 = $_POST["data2"];
+                    $data3 = $_POST["data3"];
+                    $query = ("insert into team_info values(?,?,?)");
+                    $stmt = $db->prepare($query);
+                    $stmt->execute(array($data2, $data3, $data1));
                 }
             }
         }
