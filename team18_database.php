145574c0e5987cbe61b80876c7abcebc13d67c58
pear651530 <94907326+pear651530@users.noreply.github.com>
2023-01-01 17:22:29 +0800

    Update team18_database.php

--- team18_database.php
+++ team18_database.php
@@ -1,9 +1,12 @@
 <?php
 
 $user = 'root';
-$password = 'tte123789';
-try{$db = new PDO('mysql:host=localhost;dbname=team18;charset=utf8',$user,$password);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);}
-catch(PDOException $e){Print "ERROR!:". $e->getMessage();
-die();}?>
+$password = '521071314';
+try {
+    $db = new PDO('mysql:host=localhost;dbname=team18;charset=utf8', $user, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+} catch (PDOException $e) {
+    echo "ERROR";
+    die();
+} ?>
